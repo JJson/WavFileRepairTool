68d16be62b21b8328827b8e607359b2b62128d38
linjj <515867115@qq.com>
2018-05-04 10:54:36 +0800

    添加必要注释

--- WavFileRepairTool/Classes/WavFileRepairTool.h
+++ WavFileRepairTool/Classes/WavFileRepairTool.h
@@ -9,5 +9,13 @@
 #import <Foundation/Foundation.h>
 
 @interface WavFileRepairTool : NSObject
+
+/**
+ wav文件修复
+
+ @param filePath 文件路径
+ @param sliceHandle 切片回调，在修复过程中，新文件写入操作的实时回调，如果需要对新文件进行一些耗时计算，比如sha1值的计算，可以在这个回调里update buffer，避免重复读取，节省时间
+ @return 是否成功
+ */
 +(BOOL)repairFile:(NSString *)filePath sliceHandle:(void (^)(void * dataWrited,size_t length))sliceHandle;
 @end
